8c5e475fbf2e6b9dde85e86c540f9f0bc4756121
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2018-08-24 00:55:55 +0000

    Add untested and partial implementation of codeblock

--- Sources/ZolangCore/Frontend/Models/Nodes/CodeBlock.swift
+++ Sources/ZolangCore/Frontend/Models/Nodes/CodeBlock.swift
@@ -16,6 +16,45 @@ public indirect enum CodeBlock: Node {
     case combination(CodeBlock, CodeBlock)
     
     public init(tokens: [Token], context: inout ParserContext) throws {
-        throw ZolangError.ErrorType.unknown
+        var tokens = tokens
+        context.line += tokens.trimLeadingNewlines()
+        
+        let previousLine = context.line
+        
+        guard let prefixType = tokens.prefixType() else {
+            throw ZolangError(type: .unknown,
+                              file: context.file,
+                              line: context.line)
+        }
+
+        var left: CodeBlock
+        switch prefixType {
+        case .expression:
+            left = .expression(try Expression(tokens: tokens, context: &context))
+        case .ifStatement:
+            throw ZolangError.ErrorType.unknown
+        case .modelDescription:
+            throw ZolangError(type: .unexpectedToken(.describe, nil),
+                              file: context.file,
+                              line: context.line)
+        case .variableDeclaration:
+            left = .variableDeclaration(try VariableDeclaration(tokens: tokens, context: &context))
+        case .variableMutation:
+            left = .variableMutation(try VariableMutation(tokens: tokens, context: &context))
+        case .whileLoop:
+            throw ZolangError.ErrorType.unknown
+        }
+        
+        let index = context.line - previousLine
+        
+        let line = context.line
+
+        do {
+            let right = try CodeBlock(tokens: Array(tokens.suffix(from: index)), context: &context)
+            self = .combination(left, right)
+        } catch {
+            context.line = line
+            self = left
+        }
     }
 }
