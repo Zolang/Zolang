58573f85a6e492f3722ef42be753cd53360604df
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2018-09-04 02:42:41 +0000

    Add (barely any though) initial tests for if-statement

--- Tests/ZolangTests/IfStatementTests.swift
+++ Tests/ZolangTests/IfStatementTests.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import XCTest
+import ZolangCore
+
+class IfStatementTests: XCTestCase {
+    
+    let validIfStatement1 = """
+    if (some) {
+        print("SOME")
+    } else if (another) {
+        print("ANOTHER")
+    } else {
+        print("OTHER")
+        if (true) {
+            print(👏🏻)
+        }
+    }
+    """
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testFailure() {
+
+    }
+
+    func testInitialization() {
+        do {
+            var context = ParserContext(file: "zolang.test")
+            let ifStatement = try IfStatement(tokens: Lexer().tokenize(string: validIfStatement1),
+                                              context: &context)
+            XCTAssert(ifStatement.ifList.count == 2)
+            XCTAssertNotNil(ifStatement.elseBlock)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+}
